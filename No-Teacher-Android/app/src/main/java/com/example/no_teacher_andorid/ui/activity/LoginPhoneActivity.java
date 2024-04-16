8d227d66f67a87a29ef36b4625d8c456b4de06c3
xdxn <cc3523854674@163.com>
2024-04-16 09:41:06 +0800

    注册1.0

---
+++ No-Teacher-Android/app/src/main/java/com/example/no_teacher_andorid/ui/activity/LoginPhoneActivity.java
@@ -0,0 +1,16 @@
+package com.example.no_teacher_andorid.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.no_teacher_andorid.R;
+
+public class LoginPhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_phone);
+    }
+}
