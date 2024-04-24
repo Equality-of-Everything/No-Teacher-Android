e0db1e70b317ec6babf8f274fc59d2a464981404
AuroraZLee <I217650Shaw@163.com>
2024-04-24 15:39:56 +0800

    新加了可以选择出生日期

--- No-Teacher-Android/app/src/main/java/com/example/android/ui/activity/UserRegisterActivity.java
+++ No-Teacher-Android/app/src/main/java/com/example/android/ui/activity/UserRegisterActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.android.api.ApiService;
 import com.example.android.http.retrofit.RetrofitManager;
 import com.example.android.viewmodel.UserEditViewModel;
 import com.example.no_teacher_andorid.R;
 import com.example.no_teacher_andorid.databinding.ActivityUserEditBinding;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 public class UserRegisterActivity extends AppCompatActivity {
     private UserEditViewModel userEditViewModel;
@@ -26,6 +30,33 @@ public class UserRegisterActivity extends AppCompatActivity {
         binding.setLifecycleOwner(this);
         ApiService apiService = RetrofitManager.getInstance(this).getApi(ApiService.class);
         userEditViewModel.setApiService(apiService);
+
+        MaterialDatePicker.Builder materialDateBuilder = MaterialDatePicker.Builder.datePicker();
+        materialDateBuilder.setTitleText("选择日期");
+        final MaterialDatePicker materialDatePicker = materialDateBuilder.build();
+
+        binding.pickDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker.show(getSupportFragmentManager(), "MATERIAL_DATE_PICKER");
+            }
+        });
+
+        materialDatePicker.addOnPositiveButtonClickListener(
+                new MaterialPickerOnPositiveButtonClickListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onPositiveButtonClick(Object selection) {
+
+                        // if the user clicks on the positive
+                        // button that is ok button update the
+                        // selected date
+                        binding.showSelectedDate.setText(materialDatePicker.getHeaderText());
+                        // in the above statement, getHeaderText
+                        // is the selected date preview from the
+                        // dialog
+                    }
+                });
 
     }
 }
