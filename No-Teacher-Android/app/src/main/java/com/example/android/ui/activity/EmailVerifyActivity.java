f34350143ad28acea2467608fa26c461939954cd
AuroraZLee <I217650Shaw@163.com>
2024-04-24 14:44:17 +0800

    更新了登录页面
    修改了测试下载软件会生成两个图标的问题

--- No-Teacher-Android/app/src/main/java/com/example/android/ui/activity/EmailVerifyActivity.java
+++ No-Teacher-Android/app/src/main/java/com/example/android/ui/activity/EmailVerifyActivity.java
@@ -5,6 +5,7 @@ import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -43,17 +44,42 @@ public class EmailVerifyActivity extends AppCompatActivity {
             }
         });
 
-//        binding.btnVerify.setOnClickListener(view -> {
-//            String emailInput = binding.edTxtVerifyEmail.getText().toString();
-//            if(!TextUtils.isEmpty(emailInput)){
-//                Toast.makeText(this, "请接收验证码", Toast.LENGTH_LONG).show();
-//                viewModel.setEmail(emailInput);
-//                viewModel.requestSendVerificationCode();
-//            }else {
-//                Toast.makeText(this, "请输入验证码", Toast.LENGTH_LONG).show();
-//            }
-//        });
+        binding.btnVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginOnClick(view);
+            }
+        });
     }
+
+    /**
+     * @param view:
+     * @return void
+     * @author Lee
+     * @description 点击验证登录
+     * @date 2024/4/24 10:42
+     */
+    private void loginOnClick(View view) {
+        String emailInput = binding.edTxtVerifyEmail.getText().toString();
+        String verifyCode = binding.edtTxtVerifyNumber.getText().toString();
+
+        if (!TextUtils.isEmpty(emailInput) && !TextUtils.isEmpty(verifyCode)) {
+            viewModel.setEmail(emailInput); // 设置邮箱到 ViewModel
+            viewModel.setVerifyCode(verifyCode); // 设置验证码到 ViewModel
+            viewModel.verifyVerificationCode(); // 调用 ViewModel 中的方法发送验证请求
+
+        } else {
+            Toast.makeText(this, "邮箱和验证码不能为空", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * @param view:
+     * @return void
+     * @author Lee
+     * @description 点击发送验证码
+     * @date 2024/4/24 10:42
+     */
     public void emailOnclick(View view) {
         String emailInput = binding.edTxtVerifyEmail.getText().toString();
         Log.e("EmailVerifyActivity", "Email input: " + emailInput);
@@ -61,6 +87,19 @@ public class EmailVerifyActivity extends AppCompatActivity {
             Toast.makeText(this, "请接收验证码", Toast.LENGTH_LONG).show();
             viewModel.setEmail(emailInput);
             viewModel.requestSendVerificationCode();
+
+            binding.TxtVerifyNumber.setEnabled(false);  // 禁用按钮
+
+            // 开始一个1分钟的倒计时
+            new CountDownTimer(60000, 1000) {
+                public void onTick(long millisUntilFinished) {
+                    binding.TxtVerifyNumber.setText(millisUntilFinished / 1000 + " 秒");
+                }
+                public void onFinish() {
+                    binding.TxtVerifyNumber.setEnabled(true);  // 启用按钮
+                    binding.TxtVerifyNumber.setText(R.string.Txt_verify_number);  // 重置文本
+                }
+            }.start();
         } else {
             Toast.makeText(this, "请输入验证码", Toast.LENGTH_LONG).show();
         }
