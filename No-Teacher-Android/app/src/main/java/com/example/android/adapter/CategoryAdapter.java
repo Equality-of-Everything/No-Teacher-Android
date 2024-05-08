18a66232e73bc2b0db68c17961f081a14c823370
xdxn <cc3523854674@163.com>
2024-05-08 14:36:36 +0800

    c页子fragment

--- No-Teacher-Android/app/src/main/java/com/example/android/adapter/CategoryAdapter.java
+++ No-Teacher-Android/app/src/main/java/com/example/android/adapter/CategoryAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,13 +17,21 @@ import java.util.List;
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryViewHolder>{
     private Context context;
     private List<String> categories;
+    private OnItemClickListener mListener;
+
+    // 定义一个接口用于处理item点击事件
+    public interface OnItemClickListener {
+        void onItemClick(String category);
+    }
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
 
     public CategoryAdapter(Context context, List<String> categories) {
         this.context = context;
         this.categories = categories;
     }
-    @NonNull
-    @Override
+
     public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(context).inflate(R.layout.item_library_left, parent, false);
         return new CategoryViewHolder(itemView);
@@ -32,10 +41,16 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryViewHolder>{
     public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
         String category = categories.get(position);
         holder.categoryNameTextView.setText(category);
+        holder.itemView.setOnClickListener(v ->{
+            if(mListener != null){
+                mListener.onItemClick(categories.get(position));
+            }
+        } );
     }
 
     @Override
     public int getItemCount() {
         return categories.size();
     }
+
 }
