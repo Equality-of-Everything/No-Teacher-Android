764b577e61201a316cce0271581076336b86f4a1
tcyyanxi <1972228514@qq.com>
2024-05-08 14:06:02 +0800

    新增ReadActivity

---
+++ No-Teacher-Android/app/src/main/java/com/example/android/ui/activity/ReadActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.no_teacher_andorid.R;
+
+public class ReadActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+
+
+    }
+}
